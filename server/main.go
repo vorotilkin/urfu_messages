abbadf523879a655ae0a3237fa7708f68a09e02d
Eugene <evortoilkin@mail.ru>
2024-07-31 00:50:42 +0500

    docker fix

--- server/main.go
+++ server/main.go
@@ -14,6 +14,23 @@ import (
 	"messages/pkg/http"
 )
 
+type config struct {
+	Http struct {
+		Server http.Config
+	}
+	Db database.Config
+}
+
+func newConfig(configuration *configuration.Configuration) (*config, error) {
+	c := new(config)
+	err := configuration.Unmarshal(c)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
 func main() {
 	opts := []fx.Option{
 		fx.Provide(zap.NewProduction),
